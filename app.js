ab3807270d6f6521e750705349ae000e3a1e3cf5
AbdKafawen <>
2021-05-05 10:42:27 +0000

    js basics

---
+++ app.js
@@ -0,0 +1,9 @@
+var userCar = prompt('Whats your favourite hero tinker or invoker??')
+
+if (userCar === 'tinker') {
+  document.write("<p> its tinker </p>")
+} else if (userCar === 'tinker') {
+  document.write("<p> its invoker </p>")
+} else {
+  alert('I think that a hard one')
+}
