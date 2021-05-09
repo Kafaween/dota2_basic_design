bc5a7db55f458ff1989bd269f93697834458e057
AbdKafawen <>
2021-05-09 09:09:43 +0000

    functions edit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var userHero = prompt('choose your hero tinker or invoker??')
 
+function choosing(){
+var userHero = prompt('choose your hero tinker or invoker??')
 while(userHero !== "tinker" && userHero !== "invoker"){
   var userHero = prompt ("choose again")
 }
-
 if (userHero === 'tinker') {
   document.write("<p> its tinker </p>")
 } else if (userHero === 'invoker') {
@@ -11,18 +11,21 @@ if (userHero === 'tinker') {
 } else {
   alert('you know wich one dont run away')
 }
+}
+
+choosing()
 
 
+printImg()
+
+function printImg(){
 var userG = prompt ("How many games did you win with")
-
 console.log
-
-
 for (var i = 0 ; i < userG; i++){
 document.write('<img src="https://i.imgur.com/vdAIYuE.jpg" alt="" >')
 
 }
-
+}
 
 
 
